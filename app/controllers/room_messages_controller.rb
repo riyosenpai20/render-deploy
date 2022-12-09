78f84e59b4f2b5f664f0312d2481649e0b90a7d4
riyo <riyosenpai20@gmail.com>
2022-12-09 12:16:45 +0700

    create a chatroom

---
+++ app/controllers/room_messages_controller.rb
@@ -0,0 +1,16 @@
+class RoomMessagesController < ApplicationController
+    protect_from_forgery with: :null_session
+    before_action :load_entities
+
+  def create
+    @room_message = RoomMessage.create room: @room,
+                                       message: params.dig(:room_message, :message)
+    RoomChannel.broadcast_to @room, @room_message
+  end
+
+  protected
+
+  def load_entities
+    @room = Room.find params.dig(:room_message, :room_id)
+  end
+end
